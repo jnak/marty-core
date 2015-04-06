dfb54e515796c47de8fa5467a12e1885029388c7
James Hollingworth <jamiehollingworth@gmail.com>
2015-04-06 09:49:58 +0100

    Add state sources

--- register.js
+++ register.js
@@ -4,9 +4,13 @@ var warnings = require('./lib/warnings');
 var Dispatcher = require('./lib/dispatcher');
 var Diagnostics = require('./lib/diagnostics');
 var environment = require('./lib/environment');
+var StateSource = require('./lib/stateSource');
 var getClassName = require('./lib/utils/getClassName');
+var createStateSourceClass = require('./lib/stateSource/createStateSourceClass');
 
 module.exports = function (marty) {
+  marty.registerClass('StateSource', StateSource);
+
   marty.register('logger', logger);
   marty.register('dispose', dispose);
   marty.register('warnings', warnings);
@@ -15,18 +19,28 @@ module.exports = function (marty) {
   marty.register('Dispatcher', Dispatcher);
   marty.register('Diagnostics', Diagnostics);
   marty.register('diagnostics', Diagnostics);
+  marty.register('createStateSource', createStateSource);
 
-  registerEverythingIn(environment);
-
-  function registerEverythingIn(obj) {
-    _.each(obj, function (value, key) {
-      marty.register(key, value);
-    });
-  }
+  _.each(environment, function (value, key) {
+    marty.register(key, value);
+  });
 
   function dispose() {
     Dispatcher.dispose();
     this.registry.dispose();
+  }
+
+  function createStateSource(properties) {
+    var BaseType = properties.type ? marty.stateSources[properties.type] : StateSource;
+
+    if (!BaseType) {
+      throw new Error('Unknown state source ' + properties.type);
+    }
+
+    var StateSourceClass = createStateSourceClass(properties, BaseType);
+    var defaultInstance = this.register(StateSourceClass);
+
+    return defaultInstance;
   }
 
   function register(clazz, id) {
--- test/buildMarty.js
+++ test/buildMarty.js
@@ -1,9 +1,15 @@
 var buildMarty = require('../lib/buildMarty');
+var TestSource = require('./fixtures/testSource');
 
 module.exports = function () {
   return buildMarty({
     modules: [
+      fixtures,
       require('../register')
     ]
   });
 };
+
+function fixtures(marty) {
+  marty.registerStateSource('TestSource', 'testSource', TestSource);
+}
